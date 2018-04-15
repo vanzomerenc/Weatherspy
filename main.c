c4027c4f856a14f5a2fccb898406b8a9f47475f1
Unknown <teskeys@mail.gvsu.edu>
2018-04-15 16:15:32 -0400

    Test send and receive

--- main.c
+++ main.c
@@ -1,55 +1,6 @@
 /*
- * -------------------------------------------
- *    MSP432 DriverLib - v3_21_00_05 
- * -------------------------------------------
- *
- * --COPYRIGHT--,BSD,BSD
- * Copyright (c) 2016, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
-/******************************************************************************
- * MSP432 Empty Project
- *
- * Description: An empty project that uses DriverLib
- *
- *                MSP432P401
- *             ------------------
- *         /|\|                  |
- *          | |                  |
- *          --|RST               |
- *            |                  |
- *            |                  |
- *            |                  |
- *            |                  |
- *            |                  |
- * Author: 
+
+ * Author: Sam
 *******************************************************************************/
 /* DriverLib Includes */
 #include "driverlib.h"
@@ -58,6 +9,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+#include <limits.h>
 
 #include <drv/uart.h>
 #include <drv/uart_stdio_support.h>
@@ -65,76 +18,61 @@
 #include <drv/esp8266.h>
 #include <station.h>
 
-void clockInit48MHzXTL(void) {  // sets the clock module to use the external 48 MHz crystal
+#define READ_LINE_BUFSIZE (CHAR_MAX + 1)
+#define READ_LINE_LAST_POSITION CHAR_MAX
 
-    /* Configuring pins for peripheral/crystal usage */
-    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_PJ,
-            GPIO_PIN3 | GPIO_PIN2, GPIO_PRIMARY_MODULE_FUNCTION);
-    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
-
-    CS_setExternalClockSourceFrequency(32000,48000000); // enables getMCLK, getSMCLK to know externally set frequencies
-
-    /* Starting HFXT in non-bypass mode without a timeout. Before we start
-     * we have to change VCORE to 1 to support the 48MHz frequency */
-    MAP_PCM_setCoreVoltageLevel(PCM_VCORE1);
-    MAP_FlashCtl_setWaitState(FLASH_BANK0, 2);
-    MAP_FlashCtl_setWaitState(FLASH_BANK1, 2);
-    CS_startHFXT(false);  // false means that there are no timeouts set, will return when stable
-
-    /* Initializing MCLK to HFXT (effectively 48MHz) */
-    MAP_CS_initClockSignal(CS_MCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_1);
-}
+struct uart_channel wifi_uart;
 int main(void)
 {
     /* Stop Watchdog  */
     MAP_WDT_A_holdTimer();
 
     MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+    MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN0 | GPIO_PIN1);
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN0 | GPIO_PIN1);
 
-    clockInit48MHzXTL();
-    MAP_CS_initClockSignal(CS_MCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_1);
-    MAP_CS_initClockSignal(CS_SMCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_4);
-
-    //init_clocks();
-
-
-
-    //MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN0 | GPIO_PIN1);
-    //MAP_GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN0 | GPIO_PIN1);
-
-    //uart_use_stdio_support();
-
+    init_clocks();
+    uart_use_stdio_support();
     init_station_module();
-    //Interrupt_disableSleepOnIsrExit();
     rtc_init();
-    Interrupt_enableMaster();
 
-    /**
+    Interrupt_disableSleepOnIsrExit();
+    Interrupt_enableMaster();
 
     uart_replace_standard_streams(
         (struct uart_config) {.id = 0, .baud_rate = 115200, .flags = 0},
-        (struct uart_input_config) {.complete_lines = true, .echo = true},
+        (struct uart_input_config) {.complete_lines = false, .echo = true},
         (struct uart_output_config) {});
 
-    struct uart_channel wifi_uart = uart_open(
+    wifi_uart = uart_open(
             (struct uart_config) {.id = 2, .baud_rate = 115200, .flags = 0},
-            (struct uart_input_config) {.complete_lines = true, .echo = false},
+            (struct uart_input_config) {.complete_lines = false, .echo = false},
             (struct uart_output_config) {});
 
     puts("What up!\r\n");
     puts("Test\r\n");
-
-    */
-
-    //MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P4, GPIO_PIN0 | GPIO_PIN1);
-
-    //delay_ms(1000);
+    //puts("AT\r\n");
+    MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P4, GPIO_PIN0 | GPIO_PIN1);
+    delay_ms(1000);
 
     //AtInterface wifi = at_init(wifi_uart.tx, wifi_uart.rx);
-
     //int err = at_check_alive(wifi);
-
     //printf("%d", err);
+
+    delay_ms(50);
+    char receive_buff[READ_LINE_BUFSIZE] = {'\0'};
+    fseek(wifi_uart.rx, 0, SEEK_END);
+    fputs("AT\r\n", wifi_uart.tx);
+    delay_ms(50);
+    char* ptr = NULL;
+    do
+    {
+        ptr = fgets(receive_buff, READ_LINE_BUFSIZE, wifi_uart.rx);
+        if(NULL != ptr)
+        {
+            puts(receive_buff);
+        }
+    } while(NULL != ptr);
 
     while(1)
     {
