632cf202f0c2788ad0696632da47c9fd5424611d
Chris van Zomeren <vanzomerenc@gmail.com>
2018-04-16 22:24:39 -0400

    Finished weather station web server

--- main.c
+++ main.c
@@ -63,8 +63,13 @@
 #include <drv/uart_stdio_support.h>
 #include <drv/timing.h>
 #include <drv/esp8266.h>
+#include <drv/rtc.h>
+#include <sensors/atmospheric.h>
 
 #include <generated/status.h>
+#include "station.h"
+
+
 
 int main(void)
 {
@@ -72,6 +77,12 @@ int main(void)
     MAP_WDT_A_holdTimer();
 
     init_clocks();
+    rtc_init();
+
+    struct bme280_dev atmospheric_sensor;
+    sensor_atmospheric_init(&atmospheric_sensor);
+
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
 
     MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN0 | GPIO_PIN1);
     MAP_GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN0 | GPIO_PIN1);
@@ -86,7 +97,7 @@ int main(void)
         (struct uart_input_config) {.complete_lines = true, .echo = true},
         (struct uart_output_config) {});
 
-    struct uart_channel wifi_uart = uart_open(
+    wifi_uart = uart_open(
             (struct uart_config) {.id = 2, .baud_rate = 115200, .flags = 0},
             (struct uart_input_config) {.complete_lines = true, .echo = false},
             (struct uart_output_config) {});
@@ -102,23 +113,60 @@ int main(void)
 
     int err = at_check_alive(wifi);
     err = esp8266_set_wifi_mode(wifi, WIFI_MODE_SOFT_AP_AND_STATION);
-    err = esp8266_set_hosted_ap(
+    enum esp8266_wifi_error wifi_err = 0;
+    err = esp8266_connect_to_ap(
             wifi,
             (struct wifi_ap) {
-                .ssid="Your Advertisement Here",
-                .passphrase="sendmoneyplease",
-                .channel=6},
+                .ssid="G6_5137",
+                .passphrase="TomatoSandwich"},
+            &wifi_err,
             1000);
+    do
+    {
+        err = esp8266_set_hosted_ap(
+                wifi,
+                (struct wifi_ap) {
+                    .ssid="Your Advertisement Here",
+                    .passphrase="sendmoneyplease",
+                    .channel=6},
+                1000);
+    } while(err == AT_BUSY);
 
     err = esp8266_server_start(wifi, 1000);
 
     struct webpage_status status = {0};
 
+    set_time_nist();
+    rtc_settime(&received_time);
 
-
+    int64_t last_iter_time;
     while(1)
     {
+        last_iter_time = now();
+
+        struct rtc_time ct;
+        rtc_gettime(&ct);
+        status.year = ct.year;
+        status.month = ct.month;
+        status.day = ct.date;
+        status.hour = ct.hour;
+        status.minute = ct.min;
+        status.second = ct.sec;
+
+        struct sensor_atmospheric_result current_reading;
+        sensor_atmospheric_read(&atmospheric_sensor, &current_reading);
+        status.temperature = current_reading.temperature;
+        status.humidity = current_reading.humidity;
+        status.pressure = current_reading.pressure;
+
         esp8266_server_periodic(wifi, &print_webpage_status, &status);
-        PCM_gotoLPM0();
+        clearerr(wifi_uart.rx);
+        fseek(wifi_uart.rx, 0, SEEK_END);
+        P1->OUT ^= 0x1;
+
+        while(last_iter_time + 10 > now())
+        {
+            PCM_gotoLPM0();
+        }
     }
 }
